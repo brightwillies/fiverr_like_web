b07e60799b80d3dc75f6acb4ebfe67960aaaf425
brightwillies <bwboakye2@gmail.com>
2024-09-26 21:48:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Gigs from "./pages/Gigs/Gigs";
